c456450b6a242a4069f1d77ffd9b4e40df23dceb
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-10-20 18:18:52 +0700

    update: replace header with ant menu

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,23 +1,80 @@
+import {
+  BookOutlined,
+  HomeOutlined,
+  LoginOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Menu } from 'antd';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-import './header.css';
-
 const Header = () => {
+  const [current, setCurrent] = useState('');
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  const items = [
+    {
+      label: <Link to="/">Home</Link>,
+      key: 'home',
+      icon: <HomeOutlined />,
+    },
+    {
+      label: <Link to="/users">User</Link>,
+      key: 'users',
+      icon: <UserOutlined />,
+    },
+    {
+      label: <Link to="/books">Books</Link>,
+      key: 'books',
+      icon: <BookOutlined />,
+      children: [
+        {
+          type: 'group',
+          label: 'Reference Books',
+          children: [
+            {
+              label: 'Option 1',
+              key: 'setting:1',
+            },
+            {
+              label: 'Option 2',
+              key: 'setting:2',
+            },
+          ],
+        },
+        {
+          type: 'group',
+          label: 'Textbooks',
+          children: [
+            {
+              label: 'Option 3',
+              key: 'setting:3',
+            },
+            {
+              label: 'Option 4',
+              key: 'setting:4',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: <Link to="/login">Login</Link>,
+      key: 'login',
+      icon: <LoginOutlined />,
+    },
+  ];
+
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/users">Users</NavLink>
-      </li>
-      <li>
-        <NavLink to="/books">Book</NavLink>
-      </li>
-      <li>
-        <NavLink to="/login">Login</NavLink>
-      </li>
-    </ul>
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
   );
 };
 
