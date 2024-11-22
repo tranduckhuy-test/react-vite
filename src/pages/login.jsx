4bdc4c5bbe009d5cf8df708a094b895fc364dd71
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-11-22 18:44:24 +0700

    update: update navigate when login success

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
           message: 'Success',
           description: 'Login successfully',
         });
-        navigate('/users');
+        navigate('/');
       } else {
         notification.error({
           message: 'Failed',
